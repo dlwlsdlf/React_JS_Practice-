be23dd80c3e4de0f78e6e2536c8cbac6bf111498
dlwlsdlf0723 <dlwlsdlf0723@naver.com>
2022-02-13 14:19:43 +0900

    useState, useEffect , api 사용법 연습

--- src/App.js
+++ src/App.js
@@ -3,36 +3,39 @@ import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  const [money,setMoney] =useState(0);
+  const [result,setResult] =useState(0);
+  const onChange = (event)=>setMoney(event.target.value);
+  const onSelect = (event) =>setResult(event.target.value);
+
   return (
     <div>
-      <h1>my to dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          placeholder="Write your to do ..."
-          type="text"
-        />
-        <button>Add To do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item,index) => (
-          <li key={index}>{item}</li>
+      <h1>The Coins! {loading ? "" :`(${coins.length})` }</h1>
+      {loading ? <strong>Loding...</strong> : (
+        <select onChange={onSelect}>
+        {coins.map((coin) => (
+          <option key={coin.id} value={coin.quotes.USD.price}>
+            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+          </option>
         ))}
-      </ul>
+      </select>)}
+      <input
+        value={money}
+        type="number"
+        placeholder="money (us)"
+        onChange={onChange}
+      />
+      <h2> you get ({money/result})</h2>
     </div>
   );
 }
