fe76aaf73e68ff630149c17fac8727c9a62d9809
dlwlsdlf0723 <dlwlsdlf0723@naver.com>
2022-02-13 12:34:21 +0900

    0213

--- src/App.js
+++ src/App.js
@@ -2,21 +2,38 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
-
 function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () =>setValue((prev) => prev +1);
-  console.log("i run all the time");
-
-
-  useEffect(()=>{
-    console.log("CALL THE API....");
-  }, []);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
   return (
     <div>
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>  
+      <h1>my to dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          placeholder="Write your to do ..."
+          type="text"
+        />
+        <button>Add To do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item,index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
